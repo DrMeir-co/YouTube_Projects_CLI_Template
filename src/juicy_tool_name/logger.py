5779eb5541430e01e663009ab4b25e436b93b7e7
DrMeir-co <112549679+DrMeir-co@users.noreply.github.com>
2025-12-25 18:09:17 +0200

    Add logger configuration with traceback formatting

---
+++ src/juicy_tool_name/logger.py
@@ -0,0 +1,23 @@
+import logging
+import traceback
+
+
+def configure_logger(
+    *,
+    logfile: str,
+    level: int = logging.ERROR,
+) -> logging.Logger:
+    class TracebackWithLocalsFormatter(logging.Formatter):
+        def formatException(self, exc_info):
+            tb = traceback.TracebackException.from_exception(
+                exc_info[1], capture_locals=True
+            )
+            return "".join(tb.format())
+
+    handler = logging.FileHandler(logfile, encoding="utf-8")
+    handler.setLevel(level)
+    handler.setFormatter(TracebackWithLocalsFormatter("%(levelname)s: %(message)s"))
+
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    return logger, handler
